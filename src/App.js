72aad8e69dbd91d844fa682f83fdd175d0cafb95
jesus <Jesusfernandorios93@gmail.com>
2021-07-22 16:28:58 -0700

    creating search form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-
+import { useState, useEffect } from 'react';
+import axios from 'axios'
 import './App.css';
 
+
 function App() {
+
+  const [coins, setCoins] = useState([])
+  useEffect(() => {
+    axios
+    .get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    .then(res => {
+      setCoins(res.data)
+      console.log(res.data)})
+      .catch( err => console.error(err))
+
+  }, [])
+
   return (
     <div className="App">
-      <h1>
-        Hello
-      </h1>
+      
+      <div>
+          <h1>Search A crypto currency</h1>
+          <form>
+            <input type="text" placeholder="Search"/>
+          </form>
+      </div>
+      
     </div>
   );
 }
