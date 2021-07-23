250383df8eeac487d02479170657670a6514f08d
jesus <Jesusfernandorios93@gmail.com>
2021-07-23 16:11:34 -0700

    all data is displaying well on DOM. need to add styling

--- src/App.js
+++ src/App.js
@@ -44,11 +44,11 @@ function App() {
             name={coin.name} 
             image={coin.image}
             symbol={coin.symbol}
-            volume={coin.total_voluime}
+            volume={coin.total_volume}
             price={coin.current_price}
-            priceChange={coin.price_change_percentage_24}
+            priceChange={Math.round(coin.price_change_percentage_24h * 100) / 100}
             marketCap={coin.market_cap}
-            />
+          />
         )
       })}
 
